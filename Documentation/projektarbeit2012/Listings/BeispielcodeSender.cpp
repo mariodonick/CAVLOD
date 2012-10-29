59a2ef7fa6abbea9ec14c545e16ee8731a571b8b
DatFlo <tools@serverfl.eu>
2012-10-29 18:00:27 +0100

    doc 	add anhang

---
+++ Documentation/projektarbeit2012/Listings/BeispielcodeSender.cpp
@@ -0,0 +1,18 @@
+
+#include <unistd.h> // for the sleep function
+#include <crodt.h>
+
+int main()
+{
+  crodt::SenderModuleIF* sender = new crodt::SenderModule; // creates the senderModul
+  sender->initialize();   // call initialization
+
+  // send the text string with using the timestamp flag
+  sender->sendText("abcdefghijklmnopqrstuvwxyz", true);
+
+  sleep(20); // wait until the data was sent
+
+  delete c; // delete the sender module
+
+  return 0;
+}
